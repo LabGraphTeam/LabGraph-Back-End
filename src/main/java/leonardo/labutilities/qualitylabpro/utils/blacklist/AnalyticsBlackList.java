fa871395fdb701e81a237115a8f622f73ba09299
leonardomeireles55 <leomeireles55@hotmail.com>
2025-01-30 07:15:26 -0300

    hotfix: analytics black-list fixes.

--- src/main/java/leonardo/labutilities/qualitylabpro/utils/blacklist/AnalyticsBlackList.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/utils/blacklist/AnalyticsBlackList.java
@@ -3,5 +3,5 @@ package leonardo.labutilities.qualitylabpro.utils.blacklist;
 import java.util.List;
 
 public class AnalyticsBlackList {
-    public static final List<String> BLACK_LIST = List.of("%IMG");
+    public static final List<String> BLACK_LIST = List.of("%IMG", "#IMG");
 }
