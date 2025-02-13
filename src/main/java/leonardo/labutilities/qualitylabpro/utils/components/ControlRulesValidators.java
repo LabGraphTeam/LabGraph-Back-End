7a6ce158e478d907cc471a77e98526dfa3a5646e
leonardomeireles55 <leomeireles55@hotmail.com>
2025-02-13 19:01:35 -0300

    bugfix:  fixed method calls in ControlRulesValidators

--- src/main/java/leonardo/labutilities/qualitylabpro/utils/components/ControlRulesValidators.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/utils/components/ControlRulesValidators.java
@@ -1,14 +1,13 @@
 package leonardo.labutilities.qualitylabpro.utils.components;
 
-import leonardo.labutilities.qualitylabpro.dtos.analytics.responses.AnalyticsDTO;
-import leonardo.labutilities.qualitylabpro.repositories.AnalyticsRepository;
-import leonardo.labutilities.qualitylabpro.utils.blacklist.AnalyticsBlackList;
-import org.springframework.stereotype.Component;
+import static leonardo.labutilities.qualitylabpro.utils.constants.EmailTemplate.ERROR_MESSAGE_TEMPLATE;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import static leonardo.labutilities.qualitylabpro.utils.constants.EmailTemplate.ERROR_MESSAGE_TEMPLATE;
+import org.springframework.stereotype.Component;
+import leonardo.labutilities.qualitylabpro.dtos.analytics.responses.AnalyticsDTO;
+import leonardo.labutilities.qualitylabpro.repositories.AnalyticsRepository;
+import leonardo.labutilities.qualitylabpro.utils.blacklist.AnalyticsBlackList;
 
 @Component
 public class ControlRulesValidators {
@@ -42,35 +41,35 @@ public class ControlRulesValidators {
 				continue;
 			}
 
-			var analyticsRecords =
-					analyticsRepository.findLast10ByNameAndLevel(analytic.name(), analytic.level())
-							.stream().filter(analyticsRecord -> !AnalyticsBlackList.BLACK_LIST
-									.contains(analyticsRecord.name()))
-							.toList();
+			var analyticsRecords = this.analyticsRepository
+					.findLast10ByNameAndLevel(analytic.name(), analytic.level()).stream()
+					.filter(analyticsRecord -> !AnalyticsBlackList.BLACK_LIST
+							.contains(analyticsRecord.name()))
+					.toList();
 
 			var mean = analyticsRecords.getFirst().mean();
 			var stdDev = analyticsRecords.getFirst().sd();
 			var values = analyticsRecords.stream().map(AnalyticsDTO::value).toList();
 
-			var lastAnalyticsRecords =
-					analyticsRepository.findLastByNameAndLevel(analytic.name(), analytic.level())
-							.stream().filter(analyticsEntry -> !AnalyticsBlackList.BLACK_LIST
-									.contains(analyticsEntry.name()))
-							.toList();
+			var lastAnalyticsRecords = this.analyticsRepository
+					.findLastByNameAndLevel(analytic.name(), analytic.level()).stream()
+					.filter(analyticsEntry -> !AnalyticsBlackList.BLACK_LIST
+							.contains(analyticsEntry.name()))
+					.toList();
 
 			var lastMean = lastAnalyticsRecords.getFirst().mean();
 			var lastStdDev = lastAnalyticsRecords.getFirst().sd();
 			var lastValues = lastAnalyticsRecords.stream().map(AnalyticsDTO::value).toList();
 
 
-			if (rule1_3s(lastValues, lastMean, lastStdDev)) {
+			if (this.rule1_3s(lastValues, lastMean, lastStdDev)) {
 				errors.append(String.format(ERROR_MESSAGE_TEMPLATE, "1-3s", analytic.name(),
 						analytic.level(), "One observation exceeds mean ±3 SD",
 						"Random Error. Reject run and investigate for potential systematic errors."));
 				reportedViolations.add(violationKey);
 			}
 
-			if (rule4_1s(values, mean, stdDev)) {
+			if (this.rule4_1s(values, mean, stdDev)) {
 				errors.append(String.format(ERROR_MESSAGE_TEMPLATE, "4-1s", analytic.name(),
 						analytic.level(),
 						"Four consecutive measurements exceed ±1 SD on same side of mean",
@@ -78,14 +77,11 @@ public class ControlRulesValidators {
 				reportedViolations.add(violationKey);
 			}
 
-			if (rule10x(values, mean, stdDev)) {
+			if (this.rule10x(values, mean, stdDev)) {
 				errors.append(String.format(ERROR_MESSAGE_TEMPLATE, "10x", analytic.name(),
-						analytic.level(),
-						"""
-								Ten consecutive measurements on same side of mean
-								Systematic Error. Review calibration, reagent stability, and instrument
-								maintenance. Recalibrate if necessary.
-								"""));
+						analytic.level(), "Ten consecutive measurements on same side of mean",
+						"Systematic Error. Review calibration, reagent stability, and instrument",
+						"maintenance. Recalibrate if necessary."));
 				reportedViolations.add(violationKey);
 			}
 		}
