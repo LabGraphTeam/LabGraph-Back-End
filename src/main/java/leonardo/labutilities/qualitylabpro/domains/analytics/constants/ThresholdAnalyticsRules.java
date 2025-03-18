5545d6a86654fc732f78db235dd6df2b6a487828
Leonardo Meireles <leomeireles55@hotmail.com>
2025-03-18 09:54:31 -0300

    refactor: update docker-compose port mapping; comment out unused analytic rules in RulesProviderComponent; remove redundant rules from ThresholdAnalyticsRules

--- src/main/java/leonardo/labutilities/qualitylabpro/domains/analytics/constants/ThresholdAnalyticsRules.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/domains/analytics/constants/ThresholdAnalyticsRules.java
@@ -6,5 +6,5 @@ public class ThresholdAnalyticsRules {
 
 	private ThresholdAnalyticsRules() {}
 
-	public static final List<String> RULES = List.of("+1s", "+2s", "+3s", "-1s", "-2s", "-3s");
+	public static final List<String> RULES = List.of("+2s", "+3s", "-2s", "-3s");
 }
