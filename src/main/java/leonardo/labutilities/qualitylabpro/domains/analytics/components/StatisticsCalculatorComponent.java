6fb704f0cf837441cb94ca658beedef1fc2ab4f7
Leonardo <leomeireles55@hotmail.com>
2025-04-03 14:59:56 -0300

    fix: update pom.xml dependencies and improve method naming in StatisticsCalculatorComponent

--- src/main/java/leonardo/labutilities/qualitylabpro/domains/analytics/components/StatisticsCalculatorComponent.java
+++ src/main/java/leonardo/labutilities/qualitylabpro/domains/analytics/components/StatisticsCalculatorComponent.java
@@ -12,84 +12,79 @@ import leonardo.labutilities.qualitylabpro.domains.analytics.dtos.responses.Mean
 @Component
 public final class StatisticsCalculatorComponent {
 
-        // Systematic error percentage (bias) = ((calculated mean - mean) * 100) / mean
-        // Random error percentage (inaccuracy) = 1.65 * coefficient of variation
-        // Total error percentage = Random error percentage + Systematic error
-        // percentage (bias)
-
         private StatisticsCalculatorComponent() {}
 
-        public static MeanAndStdDeviationDTO calculateMeanAndStandardDeviation(final List<AnalyticsDTO> values) {
+        public static MeanAndStdDeviationDTO calculateMeanAndStandardDeviation(List<AnalyticsDTO> values) {
                 return computeStatistics(extractRecordValues(values));
         }
 
-        public static List<Double> extractRecordValues(final List<AnalyticsDTO> records) {
+        public static List<Double> extractRecordValues(List<AnalyticsDTO> records) {
                 return records.stream().map(AnalyticsDTO::value).toList();
         }
 
-        public static double calculateCoefficientOfVariation(final double standartDeviation, final double mean) {
-                return (standartDeviation / mean) * 100;
+        public static double calculateCoefficientOfVariation(double standardDeviation, double mean) {
+                return (standardDeviation / mean) * 100;
         }
 
-        public static double calculateSistematicErrorPercentage(final double calculatedMean, final double mean) {
+        public static double calculateSystematicErrorPercentage(double calculatedMean, double mean) {
                 return ((calculatedMean - mean) * 100) / mean;
         }
 
-        public static double calculateRamdonErrorPercentage(final double coefficientOfVariation) {
+        public static double calculateRandomErrorPercentage(double coefficientOfVariation) {
                 return 1.65 * coefficientOfVariation;
         }
 
-        public static double calculateTotalErrorPercentage(final double randomErrorPercentage,
-                        final double sistematicErrorPercentage) {
-                return randomErrorPercentage + sistematicErrorPercentage;
+        public static double calculateTotalErrorPercentage(double randomErrorPercentage,
+                        double systematicErrorPercentage) {
+                return randomErrorPercentage + systematicErrorPercentage;
         }
 
         public static MeanAndStdDeviationDTO computeStatistics(final List<Double> values) {
 
-                final double calculetedMean = values.stream().mapToDouble(Double::doubleValue).average().orElse(0);
+                final double mean = values.stream()
+                                .mapToDouble(Double::doubleValue)
+                                .average().orElse(0.0);
 
-                final double calculetedStdDeviation = Math.sqrt(values.stream().mapToDouble(Double::doubleValue)
-                                .map(value -> Math.pow(value - calculetedMean, 2)).sum() / values.size());
+                final double standardDeviation = Math.sqrt(values.stream()
+                                .mapToDouble(Double::doubleValue)
+                                .map(value -> Math.pow(value - mean, 2))
+                                .sum() / values.size());
 
-                return new MeanAndStdDeviationDTO(calculetedMean, calculetedStdDeviation);
+                return new MeanAndStdDeviationDTO(mean, standardDeviation);
         }
 
-        public static ErrorStatisticsDTO calculateErrorStatistics(final List<AnalyticsDTO> analyticsList,
-                        final String defaultName, final String defaultLevel, final double defaultMean) {
-
+        public static ErrorStatisticsDTO calculateErrorStatistics(
+                        List<AnalyticsDTO> analyticsList, String defaultName, String defaultLevel, double defaultMean) {
                 MeanAndStdDeviationDTO calculatedStatistics = calculateMeanAndStandardDeviation(analyticsList);
 
-                final double inaccuracyPercetage = calculateCoefficientOfVariation(
+                final double inaccuracyPercentage = calculateCoefficientOfVariation(
                                 calculatedStatistics.standardDeviation(), calculatedStatistics.mean());
-                final double sistematicErrorPercentage =
-                                ((calculatedStatistics.mean() - defaultMean) * 100) / defaultMean;
 
-                final double randomErrorPercentage = calculateRamdonErrorPercentage(inaccuracyPercetage);
+                final double systematicErrorPercentage = calculateSystematicErrorPercentage(
+                                calculatedStatistics.mean(), defaultMean);
 
+                final double randomErrorPercentage = calculateRandomErrorPercentage(inaccuracyPercentage);
                 final double totalErrorPercentage =
-                                calculateTotalErrorPercentage(randomErrorPercentage, sistematicErrorPercentage);
+                                calculateTotalErrorPercentage(randomErrorPercentage, systematicErrorPercentage);
 
-                return new ErrorStatisticsDTO(defaultName, defaultLevel, defaultMean, inaccuracyPercetage,
-                                sistematicErrorPercentage, randomErrorPercentage, totalErrorPercentage,
-                                analyticsList.size());
+                return new ErrorStatisticsDTO(
+                                defaultName, defaultLevel, defaultMean, inaccuracyPercentage, systematicErrorPercentage,
+                                randomErrorPercentage, totalErrorPercentage, analyticsList.size());
         }
 
-        public static ComparativeErrorStatisticsDTO calculateComparativeErrorStatistics(final String defaultName,
-                        final String defaultLevel, double defaultMean, final List<AnalyticsDTO> firstAnalyticsList,
-                        final List<AnalyticsDTO> secondAnalyticsList, final List<String> monthList) {
-
+        public static ComparativeErrorStatisticsDTO calculateComparativeErrorStatistics(
+                        String defaultName, String defaultLevel, double defaultMean,
+                        List<AnalyticsDTO> firstAnalyticsList,
+                        List<AnalyticsDTO> secondAnalyticsList, List<String> monthList) {
                 ErrorStatisticsDTO firstErrorStatistics =
                                 calculateErrorStatistics(firstAnalyticsList, defaultName, defaultLevel, defaultMean);
-
                 ErrorStatisticsDTO secondErrorStatistics =
                                 calculateErrorStatistics(secondAnalyticsList, defaultName, defaultLevel, defaultMean);
 
                 final double enhancedPercentage = firstErrorStatistics.inaccuracyPercetage()
                                 - secondErrorStatistics.inaccuracyPercetage();
 
-                return new ComparativeErrorStatisticsDTO(defaultName, defaultLevel, monthList.get(0), monthList.get(1),
-                                enhancedPercentage);
-
+                return new ComparativeErrorStatisticsDTO(
+                                defaultName, defaultLevel, monthList.get(0), monthList.get(1), enhancedPercentage);
         }
-
 }
